5e06d73ff890b77915526efd2a15ade28ef3b0bd
asis-parajuli <60426026+asis-parajuli@users.noreply.github.com>
2020-05-01 15:31:12 +0545

    Create jwtInitials.js

---
+++ jwtInitials.js
@@ -0,0 +1,33 @@
+const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+const bcrypt = require("bcrypt");
+const _ = require("lodash");
+const { User } = require("../models/user");
+const express = require("express");
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Invalid Email or Password.");
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send("Invalid Email or Password.");
+
+  // here we can pass an object and what we pass is solely upto us
+  const token = jwt.sign({ _id: user._id }, "privateKey"); // here privateKey defines a private key that is used to generate digital signature and we should not use our private key here in real world application
+  res.send(token);
+});
+
+function validate(req) {
+  const schema = {
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required(),
+  };
+
+  return Joi.validate(req, schema);
+}
+
+module.exports = router;
